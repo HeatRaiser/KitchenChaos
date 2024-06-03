00bd698db01df17c5c23dbeb49f9c327b8464f90
Tudor Caraiman <caraimantudor458@gmail.com>
2024-06-03 15:45:53 +0300

    Player Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+
+    private bool isWalking;
+    
     private void Update()
     {
         Vector2 inputVector = Vector2.zero;
@@ -26,13 +29,21 @@ public class Player : MonoBehaviour
         {
             inputVector.x = 1f;
         }
-
+        
         inputVector = inputVector.normalized;
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
 
+        isWalking = moveDir != Vector3.zero;
+        
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+    
 }
