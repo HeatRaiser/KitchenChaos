9e8f0c1c7280ffb9f07bc2301b9402ab857e8836
Tudor Caraiman <caraimantudor458@gmail.com>
2024-06-04 11:28:44 +0300

    Collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask wallsLayer;
 
     private bool isWalking;
     
@@ -16,14 +17,39 @@ public class Player : MonoBehaviour
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = 0.8f;
-        bool canMove = !Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0f), moveDir, playerSize);
-
-         if (canMove)
-         {
-             transform.position += moveDir * (moveSpeed * Time.deltaTime);
-         }
+        float moveDistance = moveSpeed * Time.deltaTime;
+        var step = moveDir * moveDistance;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
         
+        bool canMove = !Physics.CapsuleCast(transform.position , transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance, wallsLayer);
+
+        if (!canMove)
+        {
+            var moveDirX = new Vector3(moveDir.x, 0, 0);
+            bool canMoveX = !Physics.CapsuleCast(transform.position , transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance, wallsLayer);
+            if (canMoveX)
+            {
+                canMove = true;
+                step = moveDirX * moveDistance;
+            }
+
+            if (!canMove)
+            {
+                var moveDirZ = new Vector3(0, 0, moveDir.z);
+                bool canMoveZ = !Physics.CapsuleCast(transform.position , transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance, wallsLayer);
+                if (canMoveZ)
+                {
+                    canMove = true;
+                    step = moveDirZ * moveDistance;
+                }
+            }
+        }
+        
+        if (canMove)
+        {
+            transform.position += step;
+        }
 
         isWalking = moveDir != Vector3.zero;
         
